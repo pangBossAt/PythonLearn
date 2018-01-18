30d7cd8da3aebcb6c0e34c2a7a81e0816c99c51e
Pangqp <13717580806@qq.com>
2018-01-18 16:56:03 +0800

    修改了文件

--- PythonPro/Python_cc.py
+++ PythonPro/Python_cc.py
@@ -20,3 +20,13 @@ class Fab(object):
  
 for n in Fab(5): 
     print(n)
+
+def read_file(fpath): 
+    BLOCK_SIZE = 1024 
+    with open(fpath, 'rb') as f: 
+        while True: 
+            block = f.read(BLOCK_SIZE) 
+            if block: 
+                yield block 
+            else: 
+                return
